0dcaf3f2d237dbc3cdb64fc2a214971e89235863
Rustem_Chinaev <chinaevrr@gmail.com>
2023-01-21 10:59:50 +0100

    Introduce example application

---
+++ containers/nr-app-example/src/test/java/org/example/ExampleApplicationTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExampleApplicationTest {
+    @Autowired
+    private ExampleApplication application;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(application);
+    }
+}
